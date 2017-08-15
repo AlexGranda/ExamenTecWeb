c2125380d5ab3e4ef0cf32c5d8e617393bed082e
Alexandra Granda <Rainini1@users.noreply.github.com>
2017-08-14 19:52:50 -0500

    Añadir archivos faltantes

---
+++ CompraPizzas/api/models/Usuario.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Alex on 14/8/2017.
+ */
+module.exports = {
+    attributes: {
+        nombres: {
+            type: "string"
+        },
+        password: {
+            type: "string"
+        },
+        fechaNacimiento: {
+            type: "date"
+        },
+        apellidos: {
+            type: "string"
+        },
+        correo: {
+            type: "string"
+        }
+    }
+};
